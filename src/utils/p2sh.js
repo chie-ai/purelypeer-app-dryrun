3f147ea1ea6cc1f342aa02cea4903a549abc76f1
unknown <bacuetesarchie@gmial.com>
2021-04-27 09:34:59 +0800

    Commit P2sh

---
+++ src/utils/p2sh.js
@@ -0,0 +1,8 @@
+import path from 'path'
+// import { CashCompiler } from 'cashscript'
+
+async function run () {
+  console.log('Path: ', path)
+  // console.log('CashCompiler: ', CashCompiler)
+}
+run()
